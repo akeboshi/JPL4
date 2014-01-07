061581a3381c9bd1c4ce517e46aa66f47d5a9ac4
灯 <@192.168.11.3>
2014-01-08 00:46:23 +0900

    syso修正

--- JPL/src/JPL/ch07/ex07_02/ConfirmData.java
+++ JPL/src/JPL/ch07/ex07_02/ConfirmData.java
@@ -11,53 +11,53 @@ class ConfirmData{
 	public static void main(String[] args) {
 		ConfirmData Data = new ConfirmData();
 		Data.byteData = Byte.MAX_VALUE;
-		System.out.println("byte <= byte : " + Data.byteData);
+		System.out.println("byte <= byte " + Data.byteData);
 		//Data.byteData = Short.MAX_VALUE;	error
 		Data.shortData = Byte.MAX_VALUE;
-		System.out.println("short <= byte: " + Data.shortData);
+		System.out.println("short <= byte " + Data.shortData);
 		Data.shortData = Short.MAX_VALUE;
-		System.out.println("short <= short: " + Data.shortData);
+		System.out.println("short <= short " + Data.shortData);
 		//Data.shortData = Integer.MAX_VALUE;	error
 		Data.intData = Byte.MAX_VALUE;
-		System.out.println("int: " + Data.intData);
+		System.out.println("int <= byte " + Data.intData);
 		Data.intData = Short.MAX_VALUE;
-		System.out.println("int: " + Data.intData);
+		System.out.println("int <= short " + Data.intData);
 		Data.intData = Integer.MAX_VALUE;
-		System.out.println("int: " + Data.intData);
+		System.out.println("int <= int " + Data.intData);
 		//Data.intData = Long.MAX_VALUE;	error
 		//Data.intData = Float.MAX_VALUE;	error
 		Data.longData = Byte.MAX_VALUE;
-		System.out.println("long: " + Data.longData);
+		System.out.println("long <= byte " + Data.longData);
 		Data.longData = Short.MAX_VALUE;
-		System.out.println("long: " + Data.longData);
+		System.out.println("long <= short " + Data.longData);
 		Data.longData = Integer.MAX_VALUE;
-		System.out.println("long: " + Data.longData);
+		System.out.println("long <= int " + Data.longData);
 		Data.longData = Long.MAX_VALUE;
-		System.out.println("long: " + Data.longData);
+		System.out.println("long <= long " + Data.longData);
 		//Data.longData = Float.MAX_VALUE;	error
 		Data.floatData = Byte.MAX_VALUE;
-		System.out.println("float: " + Data.floatData);
+		System.out.println("float <= byte " + Data.floatData);
 		Data.floatData = Short.MAX_VALUE;
-		System.out.println("float: " + Data.floatData);
+		System.out.println("float <= short " + Data.floatData);
 		Data.floatData = Integer.MAX_VALUE;
-		System.out.println("float: " + Data.floatData);
+		System.out.println("float <= int " + Data.floatData);
 		Data.floatData = Long.MAX_VALUE;
-		System.out.println("float: " + Data.floatData);
+		System.out.println("float <= long " + Data.floatData);
 		Data.floatData = Float.MAX_VALUE;
-		System.out.println("float: " + Data.floatData);
+		System.out.println("float <= float " + Data.floatData);
 		//Data.floatData = Double.MAX_VALUE;	error
 		Data.doubleData = Byte.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= byte " + Data.doubleData);
 		Data.doubleData = Short.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= short " + Data.doubleData);
 		Data.doubleData = Integer.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= int " + Data.doubleData);
 		Data.doubleData = Long.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= long " + Data.doubleData);
 		Data.doubleData = Float.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= float " + Data.doubleData);
 		Data.doubleData = Double.MAX_VALUE;
-		System.out.println("double: " + Data.doubleData);
+		System.out.println("double <= double " + Data.doubleData);
 
 	}
 
