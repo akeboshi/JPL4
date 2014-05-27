c775ab3d3d1f6ebaf17e01366a6d76edb6d2bc80
akari <akari@akari-no-air>
2014-05-27 19:01:44 +0900

    24-2

--- JPL/src/JPL/ch24/ex24_02/ShowCurrency.java
+++ JPL/src/JPL/ch24/ex24_02/ShowCurrency.java
@@ -5,17 +5,17 @@ import java.util.Locale;
 
 public class ShowCurrency {
 
-	static Locale[] locales = new Locale[] { Locale.JAPAN, Locale.KOREA,
+	static Locale[] locales = new Locale[] { Locale.JAPAN, new Locale("de_CH"),
 			Locale.CHINA, Locale.FRANCE, Locale.GERMANY, Locale.US, Locale.UK, };
 
-	static String[] ISO_4217 = new String[] { "JPY", "CNY", "EUR", "NZD",
-			"USD", "AUD", };
+	static String[] ISO_4217 = new String[] { "JPY", "CHF", "EUR", "CNY",
+			"USD", "ZWD", };
 
 	public static void main(String[] args) {
 		for (Locale locale : locales) {
 			System.out.printf(locale.toString());
 			for (String code : ISO_4217) {
-				System.out.printf("\t %s", Currency.getInstance(code)
+				System.out.printf(" %3s ", Currency.getInstance(code)
 						.getSymbol(locale));
 			}
 			System.out.printf("%n");
